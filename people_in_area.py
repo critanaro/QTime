34f71ba159f416b72c701027eb8db1e401a800fc
Caleb Robinson <csr4@rice.edu>
2020-09-19 09:20:41 -0500

    added comments for documentation

--- people_in_area.py
+++ people_in_area.py
@@ -1,4 +1,6 @@
 def people_in_rec_area(length_1, length_2, social_d):
-    area = length_1*length_2
-    max_people_in_rec = int(area/(social_d**2))
-    return (max_people_in_rec)
+    """This function's job is to tell us how many people can fit in a rectangular area. We model people as equally sized circles, with the distance between the center of two circles being the "social distance" between them. Our function takes the lengths of the rectangle and the distance between the center of two adjacent circles as inputs.
+    The circles are laid out in a rectangular grid within the rectangular area. This way, the lengths of the sides of the rectange can be given by L1=n*2r and L2 = m*2r where n and m are the number of circles and r is the radius of each circle. We also let social_d = 2r, the distane between the center of two circles."""
+    area = length_1*length_2 #We model our waiting area as a rectangle whose area we define as the product of its length and width.
+    max_people_in_rec = int(area/(social_d**2)) #We take A = n*m*social_d^2, where n*m yields the max number of people that can fit in the area. We get: n*m = number of people = area/social_d^2
+    return (max_people_in_rec) #returns the maximum number of people in the rectangular area 
