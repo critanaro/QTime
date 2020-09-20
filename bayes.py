9c151d9f674b4ebae30a1f98f8068d6c6ad0c21f
stvfng <sfeng0018@gmail.com>
2020-09-19 23:34:27 -0500

    combined my code and calebs

--- bayes.py
+++ bayes.py
@@ -8,9 +8,12 @@ import json
 
 cwd = os.getcwd()
 
-script, dataset, shape, rate = argv 
+script, dataset, shape, rate, l1, l2, social = argv 
 SHAPE = float(shape)
 RATE = float(rate)
+L1 = int(l1)
+L2 = int(l2)
+SOCIAL = float(social)
 #import matplotlib.pyplot as plt
 
 ##The point of the script is to abstract away all of the fucking math
@@ -84,4 +87,36 @@ for item in parameter_dict.items():
     item[1].append(rate)
 
 final_list = [i for sublist in parameter_dict.values() for i in sublist]
-print(str(final_list)[1:-1])
+
+def people_in_rec_area(length_1, length_2, social_d):
+    """This function's job is to tell us how many people can fit in a rectangular area. We model people as equally sized circles, with the distance between the center of two circles being the "social distance" between them. Our function takes the lengths of the rectangle and the distance between the center of two adjacent circles as inputs.
+    The circles are laid out in a rectangular grid within the rectangular area. This way, the lengths of the sides of the rectange can be given by L1=n*2r and L2 = m*2r where n and m are the number of circles and r is the radius of each circle. We also let social_d = 2r, the distane between the center of two circles."""
+    if length_1 <= 0 or length_2 <= 0 or social_d <= 0: #We want our lengths and social_d to be positive numbers.
+        raise Exception("Lengths of rectangle and the social distance should be positve numbers.") #Raises exception to inform user of mistake.
+    else:
+        area = length_1*length_2 #We model our waiting area as a rectangle whose area we define as the product of its length and width.
+        max_people_in_rec = int(area/(social_d**2)) #We take A = n*m*social_d^2, where n*m yields the max number of people that can fit in the area. We get: n*m = number of people = area/social_d^2
+        return (max_people_in_rec) #returns the maximum number of people in the rectangular area
+
+def is_it_safe(population, max):
+    if 0.8*max < population:
+        return ('r')
+    elif 0.5*max < population <= 0.8*max:
+        return ('y')
+    elif 0 <= population <= 0.5*max:
+        return ('g')
+
+def rewrite_output(ls, l1 = L1, l2 = L2, social = SOCIAL):
+    """
+    add rgy colors to output 
+    """
+    max_no = people_in_rec_area(l1, l2, social)
+    lsc = ls.copy()
+    second = []
+    for i in range(len(lsc)):
+        second.append(lsc[i])
+        second.append(is_it_safe(lsc[i], max_no))
+    #second.append(lsc[-1])
+    return second
+
+print(str(rewrite_output(final_list))[1:-1])
